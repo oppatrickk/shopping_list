59b93d645244cc938358524b5ca330c2340c04f8
John Patrick Prieto <prieto.patrick15@gmail.com>
2025-12-06 06:13:28 +0800

    feat(assets): added icons

--- lib/core/enums/custom_icon_data.dart
+++ lib/core/enums/custom_icon_data.dart
@@ -1,5 +1,13 @@
 enum CustomIconData {
-  arrowReloadVertical('assets/icons/arrow-reload-vertical.svg');
+  chevronDown('assets/icons/chevron-down.svg'),
+  all('assets/icons/all.svg'),
+  produce('assets/icons/produce.svg'),
+  meat('assets/icons/meat.svg'),
+  dairy('assets/icons/dairy.svg'),
+  pantry('assets/icons/pantry.svg'),
+  shoppingBasket('assets/icons/shopping-basket.svg'),
+  shoppingBasketConfirm('assets/icons/shopping-basket-confirm.svg'),
+  packageSearch('assets/icons/package-search.svg');
 
   const CustomIconData(this.assetLocation);
   final String assetLocation;
