d17ad7ae2119ce065af662704049423cf5e34e10
John Patrick Prieto <prieto.patrick15@gmail.com>
2025-12-06 13:32:42 +0800

    feat(home): added item indicator to categorized list

--- lib/features/home/presentation/widgets/home_shopping_item_list_categorized.dart
+++ lib/features/home/presentation/widgets/home_shopping_item_list_categorized.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_list/core/enums/custom_icon_data.dart';
+import 'package:shopping_list/core/ui/custom_icon.dart';
 import 'package:shopping_list/core/utils/extensions.dart';
 import 'package:shopping_list/core/utils/string_extension.dart';
+import 'package:shopping_list/core/utils/ui_helpers.dart';
+import 'package:shopping_list/features/home/domain/entities/shopping_cart.dart';
 import 'package:shopping_list/features/home/domain/entities/shopping_item.dart';
+import 'package:shopping_list/features/home/presentation/blocs/home_shopping_cart_cubit.dart';
 
 class HomeShoppingItemListCategorized extends StatelessWidget {
   const HomeShoppingItemListCategorized({
@@ -35,50 +41,113 @@ class HomeShoppingItemListCategorized extends StatelessWidget {
               child: child,
             );
           },
-          child: Container(
-            decoration: BoxDecoration(
-              color: context.colorScheme.surfaceContainer,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: Colors.white,
-                    ),
-                    clipBehavior: Clip.antiAlias,
-                    child: AspectRatio(
-                      aspectRatio: 1,
-                      child: Transform.scale(
-                        scale: 1.05,
-                        child: Image.asset(
-                          item.image,
-                          fit: BoxFit.cover,
-                          filterQuality: FilterQuality.high,
-                        ),
-                      ),
-                    ),
+          child: InkWell(
+            onTap: () => context.read<HomeShoppingCartCubit>().addItem(ShoppingCart(item: item, quantity: 1)),
+            child: BlocBuilder<HomeShoppingCartCubit, List<ShoppingCart>>(
+              builder: (context, cart) {
+                final isAdded = cart.any((e) => e.item.id == item.id && e.quantity > 0);
+                return Container(
+                  decoration: BoxDecoration(
+                    color: context.colorScheme.surfaceContainer,
+                    borderRadius: BorderRadius.circular(8),
+                    border: isAdded ? Border.all(color: context.colorScheme.tertiary, width: 3) : null,
                   ),
-                ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 4.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(item.title, style: context.textTheme.labelLarge?.medium),
-                      Text(
-                        StringExtension.formatMoney(item.price),
-                        style: context.textTheme.labelLarge?.medium,
+                      Stack(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(isAdded ? 0 : 4.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(isAdded ? 5 : 6),
+                                color: Colors.white,
+                              ),
+                              clipBehavior: Clip.antiAlias,
+                              child: AspectRatio(
+                                aspectRatio: 1,
+                                child: Transform.scale(
+                                  scale: 1.05,
+                                  child: Image.asset(
+                                    item.image,
+                                    fit: BoxFit.cover,
+                                    filterQuality: FilterQuality.high,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          if (cart.isNotEmpty && cart.any((e) => e.item.id == item.id)) ...[
+                            Positioned(
+                              top: 4,
+                              left: 4,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2),
+                                decoration: BoxDecoration(
+                                  color: context.colorScheme.onSurface,
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                child: Row(
+                                  children: [
+                                    CustomIcon(
+                                      icon: CustomIconData.shoppingBasket,
+                                      size: 16,
+                                      height: 16,
+                                      color: context.colorScheme.onPrimary,
+                                    ),
+                                    horizontalSpace(4),
+                                    Text(
+                                      cart.firstWhere((e) => e.item.id == item.id).quantity.toString(),
+                                      style: context.textTheme.labelLarge?.medium?.cColor(context.colorScheme.onPrimary),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 4),
+                                decoration: BoxDecoration(
+                                  color: context.colorScheme.tertiary,
+                                  borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(4),
+                                    bottomLeft: Radius.circular(8),
+                                  ),
+                                ),
+                                child: Center(
+                                  child: CustomIcon(
+                                    icon: CustomIconData.cancel,
+                                    size: 24,
+                                    height: 24,
+                                    color: context.colorScheme.onPrimary,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ],
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 4.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(item.title, style: context.textTheme.labelLarge?.medium),
+                            Text(
+                              StringExtension.formatMoney(item.price),
+                              style: context.textTheme.labelLarge?.medium,
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                );
+              },
             ),
           ),
         );
